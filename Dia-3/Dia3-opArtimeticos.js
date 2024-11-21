160aee8a19605f6d1018a7de84c9da989098946f
Luan Victor <luan_g.12@hotmail.com>
2024-11-21 20:49:11 -0300

    Aprendendo sobre os tipos de Operadores Aritméticos no Js

---
+++ Dia-3/Dia3-opArtimeticos.js
@@ -0,0 +1,42 @@
+let numUm = 3
+let numDois = 6
+let soma = numUm + numDois
+console.log(`O resultado da Soma é: ${soma}`)
+
+
+let numTres = 4
+let numQuatro = 2
+let subtracao = numTres - numQuatro
+console.log(`O resultado da Subtração é: ${subtracao}`)
+
+
+let numCinco = 5
+let numSeis = 5
+let multiplicacao = numCinco * numSeis
+console.log(`O resultado da Mltiplicação é: ${multiplicacao}`)
+
+
+let numSete = 10
+let numOito = 2
+let divisao = numSete / numOito
+let resto = numSete % numOito
+console.log(`O resultado da Divisão é: ${divisao}`)
+console.log(`O resultado de Resto é: ${resto}`)
+
+
+let numNove = 2
+let numDez = 2
+let exponenciacao = numNove ** numDez
+console.log(`O resultado da Exponenciação é: ${exponenciacao}`)
+
+
+let nome = 'Luan'
+let sobrenome = 'Victor'
+let Concatenação = nome + ' ' + sobrenome
+console.log(`O resultado da Concatenação de string é: ${Concatenação}`)
+
+
+let texto = 'Esse numero é '
+let numero = 5
+let textoNumero = texto + numero
+console.log(`O resultado da Concatenação com texto e número: ${textoNumero}`)
