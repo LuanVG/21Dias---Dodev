1b6b5fa66aace646f989e6d9746ac92c7f893825
Luan Victor <luan_g.12@hotmail.com>
2024-11-21 20:47:01 -0300

    Aprendendo sobre os tipos de Variáveis no Js

---
+++ Dia-3/Dia3-TiposdeVariaveis.js
@@ -0,0 +1,11 @@
+let tipoString = 'Luan'
+let tipoNumber = 24
+let tipoBoolTexto = 'true'
+let tipoBool = true
+
+
+console.log(`tipo da variável ${tipoString} é: ${typeof tipoString}`)
+console.log(`tipo da variável ${tipoNumber} é: ${typeof tipoNumber}`)
+console.log(`tipo da variável ${tipoBoolTexto} é: ${typeof tipoBoolTexto}`)
+console.log(`tipo da variável ${tipoBool} é: ${typeof tipoBool}`)
+
