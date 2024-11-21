54bd18470b2256d641c7a72994af5105776c519b
Luan Victor <luan_g.12@hotmail.com>
2024-11-21 20:48:02 -0300

    Aprendendo sobre as diferenças de variáveis Let e Const

---
+++ Dia-3/Dia3-let-const.js
@@ -0,0 +1,11 @@
+// Let e Const
+
+let meuNome = 'Luan Victor'
+console.log(meuNome)
+meuNome = 'Luan Gonçalves'
+console.log(meuNome)
+
+const a = 'Luan Victor'
+console.log(a)
+a = 'Luan Gonçalves'
+console.log(a)
