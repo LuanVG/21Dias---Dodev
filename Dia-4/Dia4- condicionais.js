85cfa2bc7c3bd3bcabe6a9c6f04ef103a89bdc14
Luan Victor <luan_g.12@hotmail.com>
2024-11-22 15:28:32 -0300

    Dia 4 da Dodev, aprendendo sobre as condicionais no Js

---
+++ Dia-4/Dia4- condicionais.js
@@ -0,0 +1,10 @@
+// Condições IF E ELSE
+// == Igual a, === Igual a e do mesmo tipo, > Maior que, < Menor que, >= Maior ou igual a, <= Menor ou igual a, != Diferente de, !== Diferente de e de outro tipo, && E, || OU, ! NÃO, ? : Operador ternário, switch case.
+
+
+let condicao = true; // Condição verdadeira
+if (condicao) { // Se a condição for verdadeira
+    console.log('Verdadeiro'); // Executa esse bloco de código
+} else { // Se a condição for falsa
+    console.log('Falso'); 
+}
